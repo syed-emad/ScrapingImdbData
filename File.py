fac3cdd0a6bdb60badf6f099ee974171424e00f6
syed-emad <61075900+syed-emad@users.noreply.github.com>
2020-05-15 08:16:21 +0500

    Final

--- File.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-response = requests.get("https://www.imdb.com/list/ls041677842/")
-soup = BeautifulSoup(response.content)
-print(soup)
-print("---------------")
